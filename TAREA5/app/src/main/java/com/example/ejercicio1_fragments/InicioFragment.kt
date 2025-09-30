3631e7fc9808463820a06ffd9913ee4a832fa80a
Renzo Murillo Alvarez <rmurilloa@ulasalle.edu.pe>
2025-09-30 14:04:48 -0500

    Añadiendo logica en archivo InicioFragment y logica del boton para pasar a SeleccionCOmidaFragment

--- TAREA5/app/src/main/java/com/example/ejercicio1_fragments/InicioFragment.kt
+++ TAREA5/app/src/main/java/com/example/ejercicio1_fragments/InicioFragment.kt
@@ -9,9 +9,24 @@
 package com.example.ejercicio1_fragments
 
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import android.widget.Button
 
 
-class InicioFragment : Fragment(R.layout.fragment_inicio){
+class InicioFragment : Fragment(R.layout.fragment_inicio) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val btn_inicio = view.findViewById<Button>(R.id.btn_inicio)
+
+        btn_inicio.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, SeleccionComidaFragment())
+                addToBackStack("seleccion_comida")
+                commit()
+            }
+        }
+    }
 }
--- TAREA5/app/src/main/java/com/example/ejercicio1_fragments/SeleccionComidaFragment.kt
+++ TAREA5/app/src/main/java/com/example/ejercicio1_fragments/SeleccionComidaFragment.kt
@@ -1,7 +1,16 @@
 package com.example.ejercicio1_fragments
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class SeleccionComidaFragment : Fragment(R.layout.fragment_seleccion_comida) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
