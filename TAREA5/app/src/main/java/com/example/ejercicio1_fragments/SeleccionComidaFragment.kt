fbe9058c78bf010a9246958f7de5f7ddad1c851f
Renzo Murillo Alvarez <rmurilloa@ulasalle.edu.pe>
2025-09-30 21:25:44 -0500

    Modificando SeleccionComidaFragments, añadiendo logica del bundle

--- TAREA5/app/src/main/java/com/example/ejercicio1_fragments/SeleccionComidaFragment.kt
+++ TAREA5/app/src/main/java/com/example/ejercicio1_fragments/SeleccionComidaFragment.kt
@@ -2,15 +2,58 @@ package com.example.ejercicio1_fragments
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 
 class SeleccionComidaFragment : Fragment(R.layout.fragment_seleccion_comida) {
 
+    private var comidaSeleccionada: String = ""
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val radioGroup = view.findViewById<RadioGroup>(R.id.radioGroupComida)
+        val btnSiguiente = view.findViewById<Button>(R.id.btn_Siguiente)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            comidaSeleccionada = when (checkedId) {
+                R.id.radioPizza -> "Pizza"
+                R.id.radioSalchipapa -> "Salchipapa"
+                R.id.radioEnsalada -> "Ensalada"
+                else -> ""
+            }
+        }
+
+        btnSiguiente.setOnClickListener {
+            if (comidaSeleccionada.isNotEmpty()) {
+                val bundle = Bundle()
+                bundle.putString("comida", comidaSeleccionada)
+
+                val fragment = SeleccionExtrasFragment()
+                fragment.arguments = bundle
+
+                requireActivity().supportFragmentManager.beginTransaction().apply{
+                    replace(R.id.fragment_container, fragment)
+                    addToBackStack("seleccion_extras")
+                    commit()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Selecciona un tipo de comida", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        setFragmentResultListener("pedidoActual") { requestKey, bundle ->
+            if (requestKey == "pedidoActual") {
+                val comida = bundle.getString("comida", "")
+                when (comida) {
+                    "Pizza" -> radioGroup.check(R.id.radioPizza)
+                    "Salchipapa" -> radioGroup.check(R.id.radioSalchipapa)
+                    "Ensalada" -> radioGroup.check(R.id.radioEnsalada)
+                }
+                comidaSeleccionada = comida
+            }
+        }
     }
 }
---
+++ TAREA5/app/src/main/java/com/example/ejercicio1_fragments/SeleccionExtrasFragment.kt
@@ -0,0 +1,12 @@
+package com.example.ejercicio1_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SeleccionExtrasFragment : Fragment(R.layout.fragment_seleccion_extras) {
+
+}
